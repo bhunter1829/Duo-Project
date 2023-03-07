219037065e1ab85b72a14e8678fca0818eb6037c
brenden hunter <hunterb7@gmatc.matc.edu>
2023-03-07 12:59:48 -0600

    Some Basic Files

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,3 +1,9 @@
 --h2 is typically used to setup a test database, not a prod database.
 --first, drop your tables (to reset your database for testing)
+DROP TABLE if exists Accounts;
 --then create your tables
+CREATE TABLE Accounts(
+    AccountID int,
+    FirstName varchar(255),
+    LastName varchar(255)
+);
--- target/classes/Tables.sql
+++ target/classes/Tables.sql
@@ -1,4 +1,9 @@
 --h2 is typically used to setup a test database, not a prod database.
 --first, drop your tables (to reset your database for testing)
+DROP TABLE if exists Accounts;
 --then create your tables
-afdbfdb
+CREATE TABLE Accounts(
+    AccountID int,
+    FirstName varchar(255),
+    LastName varchar(255)
+);
